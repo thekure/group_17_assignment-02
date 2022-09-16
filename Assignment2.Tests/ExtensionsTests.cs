73af9a3379f72fa62c6b13149b733a399ab2816b
Frederik Petersen <fred.p@live.dk>
2022-09-16 13:46:08 +0200

    IsSecure test

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -3,8 +3,12 @@ namespace Assignment2.Tests;
 public class ExtensionsTests
 {
     [Fact]
-    public void Test1()
+    public void IsSecure_given_itu_webiste_returns_true()
     {
+        var uri = new Uri("https://itu.dk");
 
+        var result = uri.IsSecure();
+
+        result.Should().Be(true);
     }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,4 +2,8 @@ namespace Assignment2;
 
 public static class Extensions
 {
+    public static bool IsSecure(this Uri uri)
+    {
+        throw new NotImplementedException();
+    }
 }
