53e8bcca848c5217a1c0c31c139012ce64d4b1a1
Frederik Petersen <fred.p@live.dk>
2022-09-16 13:53:17 +0200

    Implement wordCount

--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -1,6 +1,9 @@
 namespace Assignment2;
 
+using System.Text.RegularExpressions;
 public static class Extensions
 {
     public static bool IsSecure(this Uri uri) => uri.ToString().StartsWith("https");
+
+    public static int WordCount(this string str) => Regex.Matches(str, @"\w+").Count();
 }
