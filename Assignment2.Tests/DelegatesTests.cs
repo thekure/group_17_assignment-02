89b4ebdc56e2d2550a6e54085070c5ca5a4c5f02
Frederik Petersen <fred.p@live.dk>
2022-09-20 12:51:28 +0200

    reverse test and implementation

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -3,8 +3,24 @@ namespace Assignment2.Tests;
 public class DelegatesTests
 {
     [Fact]
-    public void Test1()
+    public void Reverse_given_teststr_returns_rtstset()
     {
+        Console.Clear();
+        using var writer = new StringWriter();
+        var str = "teststr";
+        Console.SetOut(writer);
+        Reverse rev = (string str) => {
+            foreach (var c in str.Reverse())
+            {
+                Console.Write(c);
+            }
+        };
 
+        rev(str);
+
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("rtstset");
     }
 }
+
+delegate void Reverse (String toBeReversed);
