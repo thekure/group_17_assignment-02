2fda99013c752b39dbfcc05f5d133ecd218dde20
Frederik Petersen <fred.p@live.dk>
2022-09-20 12:57:47 +0200

    product test and implementation

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -21,6 +21,19 @@ public class DelegatesTests
         var output = writer.GetStringBuilder().ToString().TrimEnd();
         output.Should().Be("rtstset");
     }
+
+    [Fact]
+    public void Product_when_given_4_5_returns_20()
+    {
+        Product p = (int one, int other) => one*other;
+        int one = 4;
+        int other = 5;
+
+        var result = p(one,other);
+
+        result.Should().Be(20);
+    }
 }
 
 delegate void Reverse (String toBeReversed);
+delegate int Product (int one, int other);
